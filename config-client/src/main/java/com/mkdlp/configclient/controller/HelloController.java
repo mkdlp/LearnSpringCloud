d2638f76f3512218094d068371fae531e5cedf44
mkdlp <mkdlp1994@163.com>
2019-07-20 09:41:18 +0800

    完成配置中心的代码

---
+++ config-client/src/main/java/com/mkdlp/configclient/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.mkdlp.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class HelloController {
+
+    @Value("${info.profile:error}")
+    private String profile;
+
+    @GetMapping("/info")
+    public Mono<String> hello() {
+        return Mono.justOrEmpty(profile);
+    }
+}
